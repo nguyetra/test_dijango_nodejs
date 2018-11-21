96e581f271b5c7a6f8a3bc9a5d09a52b583f6964
Gia Cat <ilove3q@gmail.com>
2018-11-21 21:39:01 +0100

    update game/models.pu

--- game/models.py
+++ game/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    gameId = models.PositiveIntegerField(primary_key = True)
+    player1 = models.CharField(max_length = 10)
+    player2 = models.CharField(max_length = 10)
+    winner = models.CharField(max_length = 10)
+    history = models.TextField()
+    note = models.CharField(max_length = 150)
